2e45e0b533149c65565f336c9de8f70c8c9696ef
guinunesandrade <119316984+guinunesandrade@users.noreply.github.com>
2022-11-30 10:11:26 -0300

    Código de cores

---
+++ draft.py
@@ -0,0 +1,19 @@
+def red_text(text):
+    x = f'\033[1;31m{text}\033[m'
+    return x
+
+
+def green_text(text):
+    x = f'\033[1;32m{text}\033[m'
+    return x
+
+
+def yellow_text(text):
+    x = f'\033[1;33m{text}\033[m'
+    return x
+
+
+def blue_text(text):
+    x = f'\033[1;34m{text}\033[m'
+    return x
+
